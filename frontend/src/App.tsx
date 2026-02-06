9f5a5c96ce73b70353cb6fa55090741d8f21d114
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2026-02-06 18:45:09 -0500

    created basic frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import { Input } from "./components/ui/input";
+import { Field } from "./components/ui/field";
+import { Button } from "./components/ui/button";
+
 export default function App() {
+    const [username, setUsername] = useState("");
+
     return (
-        <main>
-            <section>
-                hello
+        <main className="h-screen w-full">
+            <section className="h-full flex justify-center items-center">
+                <div className="">
+                    <Field orientation="horizontal">
+                        <Input 
+                            type="search" 
+                            placeholder="Enter your GitHub username" 
+                            onChange={(e) => setUsername(e.target.value)} 
+                        />
+                        <Button>Mog</Button>
+                    </Field>
+                </div>
             </section>
         </main>
     );
