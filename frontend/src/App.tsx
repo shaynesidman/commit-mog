6c12ff6ce03498bc86fb7dc2682e5c3e952d73f5
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2026-02-06 19:01:46 -0500

    working version for now

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,28 @@ import { Input } from "./components/ui/input";
 import { Field } from "./components/ui/field";
 import { Button } from "./components/ui/button";
 
+type FriendsData = {
+    username: string;
+    commits: number;
+}
+
 export default function App() {
     const [username, setUsername] = useState("");
+    const [friendsData, setFriendsData] = useState<FriendsData[]>([]);
+
+    const fetchFriends = async () => {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/${username}`);
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.error || "Failed to fetch friends");
+            }
+            setFriendsData(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         <main className="h-screen w-full">
@@ -16,8 +36,14 @@ export default function App() {
                             placeholder="Enter your GitHub username" 
                             onChange={(e) => setUsername(e.target.value)} 
                         />
-                        <Button>Mog</Button>
+                        <Button onClick={fetchFriends}>Mog</Button>
                     </Field>
+                    {friendsData.map((friend) => (
+                        <div key={friend.username}>
+                            <p>Username: {friend.username}</p>
+                            <p>Commits: {friend.commits}</p>
+                        </div>
+                    ))}
                 </div>
             </section>
         </main>
