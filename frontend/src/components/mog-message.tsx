b9390c9205538a1c9a9df985bb09abf06b101332
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2026-02-26 12:30:51 -0500

    feat: created bank of mogged messages. deep in brainrot rn

--- frontend/src/components/mog-message.tsx
+++ frontend/src/components/mog-message.tsx
@@ -9,7 +9,12 @@ interface MessageProps {
 
 const MESSAGES: Record<Tab, Array<(name: string) => string>> = {
     moggers: [
-        (name) => `${name} mogged you. Embarrassing.`,
+        (name) => `${name} mogged you worse than ASU frat leader frame mogged Clav.`,
+        (name) => `${name} prime Dillon Latham'd you. Do the dance with the pants.`,
+        (name) => `Cortisol spike incoming. ${name} commit-mogged you.`,
+        (name) => `You have to be jestermaxxing because ${name} mogged you.`,
+        (name) => `${name} mogged you. h00pify is making a video on this as we speak. Cue the bells.`,
+        (name) => `Call #2 ranked chad to avenge you because you got mogged by ${name}.`
     ],
     mogged: [
         (name) => `You mogged ${name}. Stay humble.`,
