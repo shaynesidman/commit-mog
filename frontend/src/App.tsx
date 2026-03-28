f9c7df76d75f53d13ffe05957b6aec15d10f2422
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2026-03-27 21:35:24 -0400

    feat: added sonner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,37 +3,41 @@ import { SearchBar } from "./components/search-bar";
 import { FriendsTabs } from "./components/friends-tabs";
 import { PeriodSelector } from "./components/period-selector";
 import { Spinner } from "./components/ui/spinner";
+import { Toaster} from "./components/ui/sonner";
 
 export default function App() {
     const { setUsername, period, setPeriod, isLoading, userData, moggers, mogged, equals, fetchFriends, reset } = useFriends();
 
     return (
-        <main className="w-full">
-            {!isLoading && (
-                <section className="min-h-screen mx-auto max-w-xl flex flex-col justify-center items-center gap-8">
-                    {!userData && (
-                        <div className="w-full flex flex-col gap-4 animate-in fade-in slide-in-from-top-6 duration-500">
-                            <PeriodSelector value={period} onChange={setPeriod} />
-                            <SearchBar onChange={setUsername} onSubmit={fetchFriends} />
-                        </div>
-                    )}
-                    {userData && (
-                        <FriendsTabs
-                            moggers={moggers}
-                            mogged={mogged}
-                            equals={equals}
-                            userCommits={userData.commits}
-                            period={period}
-                            onBack={reset}
-                        />
-                    )}
-                </section>
-            )}
-            {isLoading && (
-                <section className="min-h-screen mx-auto max-w-4xl flex flex-col justify-center items-center gap-8">
-                    <Spinner />
-                </section>
-            )}
-        </main>
+        <>
+            <main className="w-full">
+                {!isLoading && (
+                    <section className="min-h-screen mx-auto max-w-xl flex flex-col justify-center items-center gap-8">
+                        {!userData && (
+                            <div className="w-full flex flex-col gap-4 animate-in fade-in slide-in-from-top-6 duration-500">
+                                <PeriodSelector value={period} onChange={setPeriod} />
+                                <SearchBar onChange={setUsername} onSubmit={fetchFriends} />
+                            </div>
+                        )}
+                        {userData && (
+                            <FriendsTabs
+                                moggers={moggers}
+                                mogged={mogged}
+                                equals={equals}
+                                userCommits={userData.commits}
+                                period={period}
+                                onBack={reset}
+                            />
+                        )}
+                    </section>
+                )}
+                {isLoading && (
+                    <section className="min-h-screen mx-auto max-w-4xl flex flex-col justify-center items-center gap-8">
+                        <Spinner />
+                    </section>
+                )}
+            </main>
+            <Toaster />
+        </>
     );
 }
